e450d2e7f34dea6381c50fcb7b37a47edf35d8a5
aicha652 <aichaazlf652@gmail.com>
2024-01-16 01:24:56 -0800

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,12 +6,12 @@ import requests
 def number_of_subscribers(subreddit):
     """function that queries the Reddit API
     and returns the number of subscribers"""
-    if subreddit is None:
+    if subreddit is None or type(subreddit) is not str:
         return 0
 
     url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
-    user_agent = {"User-Agent": "advanced api project"}
+    headers = {"User-Agent": "advanced api project"}
 
-    response = requests.get(url, headers=user_agent).json()
+    response = requests.get(url, headers=headers).json()
 
     return response.get("data", {}).get("subscribers", 0)
