9382305497800b33c97303c60e20903b8a9f4c3f
aicha652 <aichaazlf652@gmail.com>
2024-01-16 05:50:31 -0800

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,15 +8,17 @@ def top_ten(subreddit):
     """function that queries the Reddit API and prints the titles
     of the first 10 hot posts listed for a given subreddit"""
 
-    if subreddit is None:
-        return None
-
-    url = "http://www.reddit.com/r/{}/hot".format(subreddit)
-    response = requests.get(url)
-
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+               'AppleWebKit/537.36 (KHTML, like Gecko) '
+               'Chrome/120.0.0.0 Safari/537.36'}
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     if response.status_code == 200:
-        response = response.json()
+        for post in response.json().get('data', {}).get('children', []):
+            print(post.get('data').get('title'))
     else:
-        return None
-
-    return response.get("data", {}).get("subscribers", None)
+        print(None)
