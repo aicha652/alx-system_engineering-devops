d465be9841aca40511600acc92454a9fad65b8db
aicha652 <aichaazlf652@gmail.com>
2024-01-16 13:11:25 -0800

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,9 @@ import requests
 def number_of_subscribers(subreddit):
     """function that queries the Reddit API
     and returns the number of subscribers"""
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+               'AppleWebKit/537.36 (KHTML, like Gecko) '
+               'Chrome/120.0.0.0 Safari/537.36'}
     url = "https://www.reddit.com/r/{0}/about.json".format(str(subreddit))
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
