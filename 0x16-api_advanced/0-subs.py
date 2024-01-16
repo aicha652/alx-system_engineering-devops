3c813d251d5930e6c2d9ffb8778db39d29d1e75a
aicha652 <aichaazlf652@gmail.com>
2024-01-16 03:09:49 -0800

    fix task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,8 @@ def number_of_subscribers(subreddit):
         return 0
 
     url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "python-requests/2.22.0"}
+
+    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
