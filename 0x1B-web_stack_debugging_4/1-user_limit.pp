c21021e2cec4842b347908a3a9e6c9db77264fba
aicha652 <aichaazlf652@gmail.com>
2024-01-25 04:45:53 -0800

    fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,11 +3,9 @@
 exec {'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/^holberton hard/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
-
 }
 
 exec {'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/^holberton soft/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
-
 }
